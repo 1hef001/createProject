5d1c3cbce7e8401d5ab00c83ac8e9b694eed82bd
S Ashwin <47474911+1hef001@users.noreply.github.com>
2020-04-05 12:18:02 +0530

    Add files via upload

---
+++ createProject/createRepo.py
@@ -0,0 +1,55 @@
+# import os
+
+from cred import username, password
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions as sexep
+
+
+def createRepo(repository_name):
+    # PATH = os.path.join(path, r'/chromedriver.exe')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe', chrome_options=chrome_options)
+
+    driver.get('https://github.com/login')
+    driver.implicitly_wait(50)
+
+    # with open('cred.txt', 'r') as f:
+    #     credentials = f.readlines()
+    #     username = credentials[0].strip()
+    #     password = credentials[1].strip()
+    #     f.close()
+
+    assert 'GitHub' in driver.title
+
+    uField = driver.find_element_by_id('login_field')
+    pField = driver.find_element_by_id('password')
+
+    uField.clear()
+    pField.clear()
+
+    uField.send_keys(username)
+    pField.send_keys(password)
+    pField.send_keys(Keys.RETURN)
+
+    driver.get('https://github.com/new')
+
+
+    repo = driver.find_element_by_id('repository_name')
+
+    repo.clear()
+
+    repo.send_keys(repository_name)
+    driver.implicitly_wait(50)
+    submit = driver.find_elements_by_xpath('//*[@id="new_repository"]/div[3]/button')[0]
+    submit.submit()
+
+    driver.implicitly_wait(50)
+    git = driver.find_element_by_id('empty-setup-clone-url')
+    gitURL = git.get_attribute('value')
+    
+    driver.quit()
+
+    return gitURL
